dfad962615546e5d06ebd0b504a1f23cb3f68ee9
Vatsal-2308 <63722388+Vatsal-2308@users.noreply.github.com>
2022-02-09 20:08:59 +0530

    Update DBUtil.java

--- src/main/java/database/DBUtil.java
+++ src/main/java/database/DBUtil.java
@@ -16,9 +16,10 @@ public class DBUtil {
         else {
             try {
                 String driver = "com.mysql.cj.jdbc.Driver";//prop.getProperty("driver");
-                String url = "jdbc:mysql://localhost:3306/project1";//prop.getProperty("url");
-                String user ="root";// prop.getProperty("user");
-                String password = "Vatsal@2308";//prop.getProperty("password");
+                String url = "postgres://wrkhsvjgqpmilx:6057826637dbb5bcb9a9b8a093465e385815f84d24d48e04d81dfc9ca9c472ae@ec2-54-156-110-139.compute-1.amazonaws.com:5432/d9og30j9emfg1v";//prop.getProperty("url");
+                String user ="wrkhsvjgqpmilx";// prop.getProperty("user");
+                String password = "
+6057826637dbb5bcb9a9b8a093465e385815f84d24d48e04d81dfc9ca9c472ae";//prop.getProperty("password");
 
                 Class.forName(driver);
 
