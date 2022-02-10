92c40ee45f0a671436005a49a8e43941d287d708
Vatsal <vatsalgujarati2000.vg@gmail.com>
2022-02-10 14:01:30 +0530

    Trying

--- src/main/java/database/DBUtil.java
+++ src/main/java/database/DBUtil.java
@@ -9,30 +9,29 @@ public class DBUtil {
 	private static Connection connection = null;
 	
 	//This function is used to set connection between the sql database and java server.
-	public static Connection getConnection() {
+	public static Connection getConnection() throws SQLException{
 		
         if (connection != null)
             return connection;
         else {
-            try {
+
                 String driver = "com.postgresql.cj.jdbc.Driver";//prop.getProperty("driver");
-                String url = "jdbc:postgresql://wrkhsvjgqpmilx:6057826637dbb5bcb9a9b8a093465e385815f84d24d48e04d81dfc9ca9c472ae@ec2-54-156-110-139.compute-1.amazonaws.com:process.env.PORT/d9og30j9emfg1v";//prop.getProperty("url");
+                String url = "jdbc:postgresql://ec2-54-156-110-139.compute-1.amazonaws.com:5432/d9og30j9emfg1v";//prop.getProperty("url");
                 String user ="wrkhsvjgqpmilx";// prop.getProperty("user");
                 String password = "6057826637dbb5bcb9a9b8a093465e385815f84d24d48e04d81dfc9ca9c472ae";//prop.getProperty("password");
 
-                Class.forName(driver);
+                //Class.forName(driver);
 
-                connection = DriverManager.getConnection(url);
+                Connection connection = DriverManager.getConnection(url,user,password);
+                if(connection!=null)
+                {
+                    System.out.println("Connection DOne");
+                }
                 
 
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (SQLException e) {
-                e.printStackTrace();
             }
 
             return connection;
         }
 
     }
-}
